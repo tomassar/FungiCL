2e4c03a6c76eea933096ea539f23e48ea93d6e47
Javier Marquez <jamarquez145@gmail.com>
2021-07-26 22:17:48 -0400

    Commit final de Javier, creo

--- src/test/java/datos/TestManejaDatosHongo.java
+++ src/test/java/datos/TestManejaDatosHongo.java
@@ -26,29 +26,54 @@ class TestManejaDatosHongo {
 
     @Test
     @DisplayName("Test para verificar conexiones fallidas")
-    void conexionFallida(){
+    void conexionFallida() {
         assertFalse(manejo.handleEstablecerConexion(null, "root", "3306"));
     }
 
     @Test
-    @DisplayName("Test para verificar creación de un hongo")
-    void crearHongo(){
+    @DisplayName("Test para verificar fallo al crear un usuario en la base de datos")
+    void falloAlCrearUsuario() {
+        manejo.handleEstablecerConexion("baseDatosPrueba", "root", "3306");
         ArrayList<TipoHongo> genericas = new ArrayList<>();
-       Hongo hongo = new Hongo("Genérico", "Genérico", "Descripción: Genérica", genericas, EstadoHongo.valueOf("POR_CONFIRMAR"),null);
+        Hongo hongo = new Hongo("Genérico", "Genérico", "Descripción: Genérica", genericas, EstadoHongo.valueOf("POR_CONFIRMAR"), null);
+        assertFalse(manejo.handleCrear(hongo));
+    }
+
+    @Test
+    @DisplayName("Test para verificar creación de un hongo en la base de datos")
+    void crearHongo() {
+        manejo.handleEstablecerConexion("baseDatosPrueba", "root", "3306");
+        ArrayList<TipoHongo> genericas = new ArrayList<>();
+        Hongo hongo = new Hongo("Genérico", "Genérico", "Descripción: Genérica", genericas, EstadoHongo.valueOf("POR_CONFIRMAR"), null);
         assertTrue(manejo.handleCrear(hongo));
     }
 
     @Test
     @DisplayName("Test para verificar fallo al cargar hongos desde la base de datos")
-    void FalloAlCargarHongos(){
-
+    void FalloAlCargarHongos() {
+        manejo.handleEstablecerConexion("baseDatosPrueba", "root", "3306");
         assertEquals(0, manejo.handleObtenerHongos().size());
     }
 
     @Test
-    @DisplayName("Test para verificar fallo al buscar hongos por nombre en la base de datos")
-    void FalloAlBuscarHongos(){
+    @DisplayName("Test para verificar carga de hongos exitosa")
+    void CargarUsuarios() {
+        manejo.handleEstablecerConexion("baseDatosPrueba1", "root", "3306");
+        assertEquals(1, manejo.handleObtenerHongos().size());
+    }
 
+    @Test
+    @DisplayName("Test para verificar fallo al buscar hongos por nombre en la base de datos")
+    void FalloAlBuscarHongos() {
+        manejo.handleEstablecerConexion("baseDatosPrueba1", "root", "3306");
         assertEquals(0, manejo.handleBuscarHongoPorNombre("inexistente").size());
+    }
+
+    @Test
+    @DisplayName("Test para verifica busqueda de hongos por nombre en la base de datos")
+    void BuscarHongos() {
+
+        manejo.handleEstablecerConexion("baseDatosPrueba1", "root", "3306");
+        assertEquals(1, manejo.handleBuscarHongoPorNombre("Genérico").size());
     }
 }
--- src/test/java/datos/TestManejaDatosUsuario.java
+++ src/test/java/datos/TestManejaDatosUsuario.java
@@ -30,34 +30,46 @@ public class TestManejaDatosUsuario {
     }
 
     @Test
-    @DisplayName("Test para verificar fallo al crear un usuario")
+    @DisplayName("Test para verificar fallo al crear un usuario en la base de datos")
     void falloAlCrearUsuario() {
+        manejo.handleEstablecerConexion("baseDatosPrueba", "root", "3306");
         Usuario usuario = new Usuario(1, "Genérico", "contrasenia", "generico@generico.com");
         assertFalse(manejo.handleCrear(usuario));
     }
 
     @Test
-    @DisplayName("Test para verificar creación de un usuario exitosa")
+    @DisplayName("Test para verificar creación de un usuario exitosa en la base de datos")
     void crearUsuario() {
+            manejo.handleEstablecerConexion("baseDatosPrueba1", "root", "3306");
             Usuario usuario = new Usuario("genérico", "contrasenia", "generico@generico.com");
-            String a= "genérico";
-            assertEquals(a, usuario.getNombre());
+            assertTrue(manejo.handleCrear(usuario));
     }
 
     @Test
     @DisplayName("Test para verificar fallo al cargar usuarios desde la base de datos")
     void falloAlCargarUsuarios() {
+        manejo.handleEstablecerConexion("baseDatosPrueba", "root", "3306");
         assertEquals(0, manejo.handleObtenerUsuarios().size());
     }
+
     @Test
     @DisplayName("Test para verificar carga de usuarios exitosa")
-    void cargarUsuarios() {
-        assertEquals(0, manejo.handleObtenerUsuarios().size());
+    void CargarUsuarios() {
+        manejo.handleEstablecerConexion("baseDatosPrueba1", "root", "3306");
+        assertEquals(1, manejo.handleObtenerUsuarios().size());
     }
 
     @Test
     @DisplayName("Test para verificar fallo al iniciar sesión")
     void falloAlBuscarHongos() {
+        manejo.handleEstablecerConexion("baseDatosPrueba", "root", "3306");
         assertNull(manejo.handleIniciarSesion("inexistente@generico.com", "contrasenia"));
+    }
+
+    @Test
+    @DisplayName("Test para verificar inicio de sesión exitosa")
+    void BuscarHongos() {
+        manejo.handleEstablecerConexion("baseDatosPrueba1", "root", "3306");
+        assertNull(manejo.handleIniciarSesion("generico@generico.com", "contrasenia"));
     }
 }
